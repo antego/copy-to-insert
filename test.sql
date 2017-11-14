53ec728ffd9d49a0114c95520fbef15de8957a65
antego <antego@users.noreply.github.com>
2017-11-14 23:02:59 +0300

    convert default dump

---
+++ test.sql
@@ -0,0 +1,20 @@
+COPY permissions (ship, name, value) FROM stdin;
+admin	admin	true
+admin	browse_as	true
+\.
+
+COPY x (a, b, c, d, e) from stdin;
+9999	\N	\\N	\NN	\N
+10000	21	31	41	51
+\.
+
+COPY x (b, d) from stdin;
+1	test_1
+\.
+
+COPY x (b, d) from stdin;
+2	test_2
+3	test_3
+4	test_4
+5	test_5
+\.
